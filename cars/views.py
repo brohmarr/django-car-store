b1a4501aa33c4a14829cfc899a8db5b14ad14b06
Thiago Panza <panza.thiagoj@gmail.com>
2024-10-29 19:46:35 -0300

    Add initial (static) DTL

--- cars/views.py
+++ cars/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 
 def cars_view(request):
-    return render(request, 'cars.html')
+    return render(request,
+                  'cars.html',
+                  {'cars': ['Marea', 'Civic', 'Corolla']}
+    )
