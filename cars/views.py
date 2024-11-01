cfdb251b7d0c8c1a06aca2b2cb180f3056af24d0
Thiago Panza <panza.thiagoj@gmail.com>
2024-10-31 23:15:05 -0300

    Add filter option to cars and ordered output

--- cars/views.py
+++ cars/views.py
@@ -4,6 +4,10 @@ from cars.models import Car
 
 def cars_view(request):
     cars = Car.objects.all()
+    search = request.GET.get('search')
+
+    if search:
+        cars = cars.filter(model__icontains=search).order_by('brand__name')
 
     return render(request,
                   'cars.html',
