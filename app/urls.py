a927c9e4b04766f171929bac6e9d71895ba7ca40
brohmarr <panza.thiagoj@gmail.com>
2024-11-26 16:15:18 -0300

    Update templates style and code cleanup

--- app/urls.py
+++ app/urls.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from cars.views import homepage, cars_view, about
+from cars.views import home, cars_view, about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', homepage, name='homepage'),
+    path('', home, name='home'),
     path('cars/', cars_view, name='cars_list'),
     path('about/', about, name='about')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- cars/views.py
+++ cars/views.py
@@ -3,7 +3,7 @@ from django.db.models import Q
 from cars.models import Car
 import locale
 
-# Set the locale for formatting.
+# Set the locale for the function below.
 locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
 
 def format_currency(value):
@@ -11,32 +11,27 @@ def format_currency(value):
     return locale.format_string("%d", value, grouping=True)
 
 
-def homepage(request):
-    return render(request, 'homepage.html')
+def home(request):
+    return render(request, 'home.html')
 
 
 def cars_view(request):
-    # Get all available cars in the database ordered by brand and value.
     cars = Car.objects.all().order_by('brand__name', 'value')
 
     for car in cars:
         car.value = format_currency(car.value)
 
-    # Get the search parameter from the user.
     search_query = request.GET.get('search')
 
-    # Filter results by model and brand.
     if search_query:
         cars = cars.filter(Q(model__icontains=search_query) | 
                            Q(brand__name__icontains=search_query))
 
-    # Set the page's context.
     context = {
         'cars': cars,
         'search_query': search_query
     }
 
-    # Rendering the page.
     return render(request, 'cars.html', context)
 
 
